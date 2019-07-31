f0557b5a4f14d2e0b19ee1ab6fc6477354d8b908
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-07-31 18:45:58 -0400

    created autocomplete component

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from "react";
+import ReactAutocomplete from "react-autocomplete";
 
-const InputBox = () => (
-    <div>
-    </div>
-)
+class InputBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      town: "",
+      value: ""
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <ReactAutocomplete
+          inputProps={{ style: { width: "100%", height: "30px" } }}
+          wrapperStyle={{ maxWidth: 200, height: "30px", margin: "10px" }}
+          items={this.props.towns.towns}
+          shouldItemRender={(item, value) =>
+            item.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+          }
+          renderMenu={(items, value) => (
+            <div className="autocomplete__menu">
+              {value === "" ? "" : items.slice(0, 3)}
+            </div>
+          )}
+          getItemValue={item => item.name}
+          renderItem={(item, highlighted) => (
+            <div
+              key={item.name}
+              style={{ backgroundColor: highlighted ? "#eee" : "transparent" }}
+            >
+              {item.name}
+            </div>
+          )}
+          value={this.state.value}
+          onChange={e => this.setState({ value: e.target.value })}
+          onSelect={value => this.setState({ value })}
+        />
+      </div>
+    );
+  }
+}
 
-export default InputBox
+export default InputBox;
