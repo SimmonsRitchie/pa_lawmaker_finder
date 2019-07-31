6edfba8a425057c89a18e424a6fee982f936a182
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-07-31 18:46:53 -0400

    connecting with json file...

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Header from './Header'
 import Footer from './Footer'
 import InputBox from './InputBox'
+import towns from '../../public/static/test.json';
+
 
 export const Main = () => (
     <div className="container__main">
       <Header />
+      <InputBox towns={towns}/>
       <Footer />
     </div>
 )
