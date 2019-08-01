e95a2f29f954c42deb5c08d33a2a696bd07bf9ed
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-07-31 23:56:00 -0400

    tweaks

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -13,31 +13,33 @@ class InputBox extends React.Component {
   render() {
     return (
       <div>
-        <ReactAutocomplete
-          inputProps={{ style: { width: "100%", height: "30px" } }}
-          wrapperStyle={{ maxWidth: 200, height: "30px", margin: "10px" }}
-          items={this.props.towns.towns}
-          shouldItemRender={(item, value) =>
-            item.name.toLowerCase().indexOf(value.toLowerCase()) > -1
-          }
-          renderMenu={(items, value) => (
-            <div className="autocomplete__menu">
-              {value === "" ? "" : items.slice(0, 3)}
-            </div>
-          )}
-          getItemValue={item => item.name}
-          renderItem={(item, highlighted) => (
-            <div
-              key={item.name}
-              style={{ backgroundColor: highlighted ? "#eee" : "transparent" }}
-            >
-              {item.name}
-            </div>
-          )}
-          value={this.state.value}
-          onChange={e => this.setState({ value: e.target.value })}
-          onSelect={value => this.setState({ value })}
-        />
+        <div className="autocomplete__wrapper">
+          <ReactAutocomplete
+            inputProps={{ style: { width: "100%", height: "30px" } }}
+            wrapperStyle={{ maxWidth: 200, height: "30px", margin: "10px" }}
+            items={this.props.towns.towns}
+            shouldItemRender={(item, value) =>
+              item.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+            }
+            renderMenu={(items, value) => (
+              <div className="autocomplete__menu">
+                {value === "" ? "" : items.slice(0, 3)}
+              </div>
+            )}
+            getItemValue={item => item.name}
+            renderItem={(item, highlighted) => (
+              <div
+                key={item.name}
+                style={{ backgroundColor: highlighted ? "#eee" : "transparent" }}
+              >
+                {item.name}
+              </div>
+            )}
+            value={this.state.value}
+            onChange={e => this.setState({ value: e.target.value })}
+            onSelect={value => this.setState({ value })}
+          />
+        </div>
       </div>
     );
   }
