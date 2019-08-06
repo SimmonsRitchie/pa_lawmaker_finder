12f1c7699d42eb7a105f84b68dae3a8678830296
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-06 17:09:36 -0400

    removed stale dependencies and removed redux stuff

--- src/app.js
+++ src/app.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'; // We use this component to connect redux to react
-// import 'normalize.css/normalize.css'; // commenting out because using bulma
 import './styles/styles.scss';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
-import 'react-dates/lib/css/_datepicker.css';
 import Main from './components/Main'
-import configureStore from './store/configureStore';
 import Loader from './components/Loader'
 
-// CALLING STORE
-const store = configureStore();
-
-// CONNECTING STORE TO PROVIDER
-// By wrapping 'AppRouter' component with 'Provider' and passing in store
-// We're able to access redux store across all child components in AppRouter.
-// We use 'Connect' in those components to grab info from store.
-const jsx = (
-    <Provider store={store}>
-        <Main />
-    </Provider>
-);
 
 // RENDERAPP
 // Function below checks to see whether we have already rendered the page
@@ -28,7 +12,7 @@ const jsx = (
 let hasRendered = false;
 const renderApp = () => {
     if (!hasRendered) {
-        ReactDOM.render(jsx, document.getElementById('app'))
+        ReactDOM.render(<Main />, document.getElementById('app'))
         hasRendered = true;
     }
 };
