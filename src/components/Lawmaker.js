18b19cffbe1aa07c1fd2bdbd8d12e1ecf52c1e49
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-11 14:38:26 -0400

    improve table appearance

--- src/components/Lawmaker.js
+++ src/components/Lawmaker.js
@@ -1,18 +1,17 @@
 import React from "react";
 
-function phoneLink (phNumber) {
+function phoneLink(phNumber) {
   // make sure input not undefined and is string
-  if (!(typeof phNumber === 'string') || !phNumber) {
-    return phNumber
+  if (!(typeof phNumber === "string") || !phNumber) {
+    return phNumber;
   }
   // make sure input is valid US phone number
-  const pat = /(\+?1-?)?\d{3}-?\d{3}-?\d{3}/
+  const pat = /(\+?1-?)?\d{3}-?\d{3}-?\d{3}/;
   if (pat.test(phNumber)) {
-    return (<a href={`tel:+1-${phNumber}`}>{phNumber}</a>)
+    return <a href={`tel:+1-${phNumber}`}>{phNumber}</a>;
   }
-  return phNumber
+  return phNumber;
 }
-
 
 class Lawmaker extends React.Component {
   render() {
@@ -32,23 +31,60 @@ class Lawmaker extends React.Component {
     } = this.props;
 
     const chamberDistrict = position === "Sen." ? "SD" : "HD";
-    const partyClass = party === "R" ? "lawmaker__table-container--R" : "lawmaker__table-container--D"
+    const chamber = position === "Sen." ? "Senate" : "House";
+    const partyClass =
+      party === "R"
+        ? "lawmaker__table-container--R"
+        : "lawmaker__table-container--D";
     return (
       <div className="box lawmaker__container">
-        <div className="">
-          <h2>
-            {position} {first_name} {last_name} ({party}, {chamberDistrict} {district})
-          </h2>
-          <table className="table is-striped">
-            <tbody className="is-size-7">
-              {harrisburg_office_phone && <TableRow name="Capitol Ph" value={phoneLink(harrisburg_office_phone)} />}
-              {district_office_phone && <TableRow name="District Ph" value={phoneLink(district_office_phone)} />}
-              {email && <TableRow name="Email" value={<a href={"mailto:" + email}>{email}</a>} />}
-              {harrisburg_office_fax && <TableRow name="Harrisburg Office Fax" value={harrisburg_office_fax} />}
-              {district_office_address && <TableRow name="District Office Address" value={`${district_office_address}, ${city}, PA ${zip}`} />}
-            </tbody>
-          </table>
-        </div>
+        <table className="table is-striped is-narrow is-hoverable is-fullwidth">
+          <thead>
+            <tr>
+            <th colSpan="2" className={`lawmaker__th`}>
+                <div className={`is-size-6 ${partyClass}`}>
+                  {position} {first_name} {last_name} ({party})
+                </div>
+
+                <div className="is-size-7 has-text-grey-dark has-text-weight-light">
+                  {chamber} District {district}
+                </div>
+            </th>
+            </tr>
+          </thead>
+          <tbody className="is-size-7">
+            {harrisburg_office_phone && (
+              <TableRow
+                name="Capitol Ph"
+                value={phoneLink(harrisburg_office_phone)}
+              />
+            )}
+            {district_office_phone && (
+              <TableRow
+                name="District Ph"
+                value={phoneLink(district_office_phone)}
+              />
+            )}
+            {email && (
+              <TableRow
+                name="Email"
+                value={<a href={"mailto:" + email}>{email}</a>}
+              />
+            )}
+            {harrisburg_office_fax && (
+              <TableRow
+                name="Harrisburg Office Fax"
+                value={harrisburg_office_fax}
+              />
+            )}
+            {district_office_address && (
+              <TableRow
+                name="District Office Address"
+                value={`${district_office_address}, ${city}, PA ${zip}`}
+              />
+            )}
+          </tbody>
+        </table>
       </div>
     );
   }
@@ -57,12 +93,10 @@ class Lawmaker extends React.Component {
 // <div className="lawmaker__container">
 // <div className={`lawmaker__table-container`}>
 
-
 export default Lawmaker;
 
-const TableRow = ({name, value}) => (
+const TableRow = ({ name, value }) => (
   <tr>
-    <td><b>{name}:</b></td> <td>{value}</td>
+    <td>{name}:</td> <td>{value}</td>
   </tr>
-  )
-
+);
