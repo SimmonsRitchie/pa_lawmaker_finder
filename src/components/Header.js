d423f0e09853b80d0018cd6d2c13804e7bd5c6dd
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-13 14:15:32 -0400

    add Spotlight Pa logo, adjust styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Header = () => (
     <div className="header__container">
-      <h2 className="title is-2 has-text-centered has-text-weight-bold is-size-4">Contact your local lawmakers</h2>
+      <div className="header__container-inner">
+        <div>
+          <h2 className="title is-4 has-text-centered has-text-weight-bold">Contact your local lawmakers</h2>
+        </div>
+        <img className="header__logo" src="/images/logo.png" />
+      </div>
     </div>
 )
 
