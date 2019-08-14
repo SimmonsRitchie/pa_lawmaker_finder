b687bb50bd78bb52ef2400125395bb67f4de0918
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-14 11:44:54 -0400

    further adjustments

--- src/utils/handlePym.js
+++ src/utils/handlePym.js
@@ -13,19 +13,21 @@ export const pymSendHeight = ({timeout=0}={}) => {
   // INTERSECTION OBSERVER
   if ('IntersectionObserver' in window) {
     // supported
-    console.log("Intersection observer is supported")
     let observer = new IntersectionObserver((entries) => {
       const elemVisib = entries[0].intersectionRatio
-      if (elemVisib >= 0 && elemVisib < 0.5) {
-        console.log("Less than a half of app is visible")
+      if (elemVisib >= 0 && elemVisib < 0.4) {
+        console.log("Less than 0.4 of app is visible")
         console.log("Scrolling to top...")
-        document.getElementById("app").scrollIntoView(false);
+        document.getElementById("app").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
         console.log("Scrolled to top")
-      } else if (elemVisib >= 0.3) {
-        console.log("More than half of app is visible")
+      } else if (elemVisib >= 0.6) {
+        console.log("More than 0.6 of app is visible")
       }
     });
+    console.log("Intersection observer connected")
     observer.observe(document.querySelector('#app'))
+    observer.disconnect()
+    console.log("Intersection observer disconnected")
 } else {
     console.log("Intersection is not supported")
 }
