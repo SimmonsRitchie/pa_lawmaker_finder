284b97a2118bdae30bd2deabce062052bdce71bf
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-14 12:03:09 -0400

    further tweaks

--- src/utils/handlePym.js
+++ src/utils/handlePym.js
@@ -1,6 +1,6 @@
 // Handles pym sendHeight and other functionality if needed
 
-export const pymSendHeight = ({timeout=0}={}) => {
+export const pymSendHeight = ({timeout=0, checkHeight=true}={}) => {
   /* Set options in object. Params:
   timeout: int. Sets milliseconds before pymChild sends height to parent.
   defaults to 0. 
@@ -10,27 +10,32 @@ export const pymSendHeight = ({timeout=0}={}) => {
   // console.log("Scrolling to app...")
   // document.getElementById("app").scrollIntoView(false);
 
+  if (checkHeight) {
+    checkAppIsVisible()
+  }
+}
+
+const checkAppIsVisible = () => {
   // INTERSECTION OBSERVER
   if ('IntersectionObserver' in window) {
     // supported
     let observer = new IntersectionObserver((entries) => {
       const elemVisib = entries[0].intersectionRatio
-      if (elemVisib >= 0 && elemVisib < 0.4) {
-        console.log("Less than 0.4 of app is visible")
+      if (elemVisib < 0.3) {
+        console.log("Less than 0.3 of app is visible")
         console.log("Scrolling to top...")
         document.getElementById("app").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
         console.log("Scrolled to top")
-      } else if (elemVisib >= 0.6) {
+      } else if (elemVisib >= 0.7) {
         console.log("More than 0.6 of app is visible")
       }
+      observer.disconnect()
+      console.log("Intersection observer disconnected")
     });
     console.log("Intersection observer connected")
     observer.observe(document.querySelector('#app'))
-    observer.disconnect()
-    console.log("Intersection observer disconnected")
-} else {
-    console.log("Intersection is not supported")
-}
-
+  } else {
+      console.log("Intersection is not supported")
+  }
 }
 
