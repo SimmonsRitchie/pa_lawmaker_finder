7f2f444be3927de232602d04e12ee9d8a41b6bd6
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-14 10:59:55 -0400

    add intersection observer

--- src/utils/handlePym.js
+++ src/utils/handlePym.js
@@ -1,5 +1,10 @@
 // Handles pym sendHeight and other functionality if needed
 
+const scrollToTop = (changes, observer) => {
+  console.log("APP isn't visible - scroll to top")
+  document.getElementById("app").scrollIntoView(false);
+}
+
 export const pymSendHeight = ({timeout=0}={}) => {
   /* Set options in object. Params:
   timeout: int. Sets milliseconds before pymChild sends height to parent.
@@ -9,5 +14,21 @@ export const pymSendHeight = ({timeout=0}={}) => {
   setTimeout(() => pymChild.sendHeight(), timeout)
   // console.log("Scrolling to app...")
   // document.getElementById("app").scrollIntoView(false);
+
+  // INTERSECTION OBSERVER
+  if ('IntersectionObserver' in window) {
+    // supported
+    console.log("Intersection observer is supported")
+    const options = {
+      root: document.querySelector('#app'), // relative to document viewport 
+      rootMargin: '0px', // margin around root. Values are similar to css property. Unitless values not allowed
+      threshold: 0.1 // visible amount of item shown in relation to root
+    };
+    let observer = new IntersectionObserver(scrollToTop, options);
+    observer.observe(document.querySelector('#app'))
+} else {
+    console.log("Intersection is not supported")
+}
+
 }
 
