ba5526b2a1013e941b4dfbe793988a331b15a7a4
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-20 15:15:19 -0400

    reposition logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react';
 const Footer = () => (
     <div>
-        <div className="content has-text-centered is-size-7">
-          <p className={""}>
-          <b>Source: </b>Pa. Legislative Data Processing Center.
-          </p>
-          <p className={""}>
-            Created by Daniel Simmons-Ritchie, Spotlight PA
-          </p>
+        <div className="footer__container">
+          <div className="is-size-7">
+            <p className={""}>
+            <b>Source: </b>Pa. Legislative Data Processing Center.
+            </p>
+            <p className={""}>
+              Created by Daniel Simmons-Ritchie, Spotlight PA
+            </p>
+          </div>
+          <img className="logo" src="images/logo.png" />
         </div>
     </div>
 )
