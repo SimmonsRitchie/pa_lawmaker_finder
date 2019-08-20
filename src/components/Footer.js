bbc3bb864564613b0ba2b02eb7df41dbe611c713
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-20 17:49:04 -0400

    change visibility

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@ const Footer = () => (
     <div>
         <div className="footer__container">
           <div className="is-size-7 has-text-grey">
-            <p className={""}>
+            <p>
             <b>Source: </b>Pa. Legislative Data Processing Center.
             </p>
-            <p className={""}>
+            <p>
               Created by Daniel Simmons-Ritchie, Spotlight PA
             </p>
           </div>
-          <div className="logo__container">
+          <div className="logo__container hide-for-smaller-than-iphone6">
             <img className="logo" src="images/logo.png" />
           </div>
         </div>
