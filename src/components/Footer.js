ec8df62ae08d0da4e98af0d2f1ca74ca39325b4c
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-21 15:58:33 -0400

    add spotlight website links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
-const Footer = () => (
+const Footer = () => {
+  const spotlight = "https://www.spotlightpa.org/"
+  return (
     <div>
         <div className="footer__container">
           <div className="is-size-7 has-text-grey">
@@ -7,15 +9,15 @@ const Footer = () => (
             <b>Source: </b>Pa. Legislative Data Processing Center.
             </p>
             <p>
-              Created by Daniel Simmons-Ritchie, Spotlight PA
+              Created by Daniel Simmons-Ritchie, <a href={spotlight}>Spotlight PA</a>
             </p>
           </div>
           <div className="logo__container hide-for-smaller-than-iphone6">
-            <img className="logo" src="images/logo.png" />
+            <a href={spotlight}><img className="logo" src="images/logo.png" /></a>
           </div>
         </div>
     </div>
-)
+)}
 
 
 export default Footer
