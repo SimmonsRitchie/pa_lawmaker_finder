96c2be6913872af4626a2cd5304faf10a42239ba
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-08-31 12:54:30 -0400

    swap nomatim for google geocoder API

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,52 +1,59 @@
+const API_KEY = process.env.GOOGLE_API_KEY;
 
-const Nominatim = require('nominatim-geocoder')
-import { msg } from '../constants/displayMsg'
-import { checkPointWithinGeography } from './findWithin'
+var googleMapsClient = require("@google/maps").createClient({
+  key: API_KEY,
+  Promise: Promise // 'Promise' is the native constructor.
+});
+import { msg } from "../constants/displayMsg";
+import { checkPointWithinGeography } from "./findWithin";
 
+const getCoords = (
+  location,
+  setDistricts,
+  setMessage,
+  setLoader,
+  setButtonBox
+) => {
+  const { street, city, postalcode, state } = location;
+  const inputLocation = `${street}, ${city} ${postalcode} ${state}`; // may use in future
+  setLoader(true);
+  setMessage(msg.SEARCHING_FOR_LAWMAKERS, { italic: true });
+  googleMapsClient
+    .geocode({ address: inputLocation })
+    .asPromise()
+    .then(response => {
+      setLoader(false);
+      // Geocoder returns an array of the best matches, we take the first.
+      const bestMatch = response.json.results[0].geometry.location;
+      const inputLat = bestMatch.lat;
+      const inputLng = bestMatch.lng;
+      const houseDistrict = checkPointWithinGeography({
+        inputLat,
+        inputLng,
+        bounds: "house"
+      });
+      const senDistrict = checkPointWithinGeography({
+        inputLat,
+        inputLng,
+        bounds: "senate"
+      });
+      if (senDistrict == undefined || houseDistrict == undefined) {
+        setMessage(msg.LOCATION_OUTSIDE_DISTRICTS_GEOCODE, {
+          icon: "exclamation"
+        });
+        setButtonBox(true);
+      } else {
+        setDistricts(houseDistrict, senDistrict);
+        setMessage(msg.SUCCESS_GEOCODE, { icon: "success" });
+      }
+      return;
+    })
+    .catch(error => {
+      setLoader(false);
+      setMessage(msg.LOCATION_NOT_FOUND, { icon: "exclamation" });
+      setButtonBox(true);
+      return;
+    });
+};
 
-/* Nomatim takes as params either {q: "123 Fake St, Fakeville, Pa, USA"} for free form search or
-{
-  street:"123 Fake St", 
-  city:"Fakeville" ,
-  county: "Fake County",
-  state: "PA", 
-  country:"USA", 
-  postalcode:"11111"
-}
-*/
-
-const getCoords = (location, setDistricts, setMessage, setLoader, setButtonBox) => {
-  const {street, city, postalcode, state} = location
-  const inputLocation = `${street}, ${city} ${postalcode} ${state}` // may use in future
-  setLoader(true)
-  setMessage(msg.SEARCHING_FOR_LAWMAKERS, {italic: true})
-  const geocoder = new Nominatim({
-    secure: true, // enables ssl
-  })
-  geocoder.search( location )
-      .then((response) => {
-        setLoader(false)
-        // Geocoder returns an array of the best matches, we take the first.
-        const bestMatch = response[0]
-          const inputLat = bestMatch.lat
-          const inputLng = bestMatch.lon
-          const houseDistrict = checkPointWithinGeography({inputLat, inputLng, bounds:"house"})
-          const senDistrict = checkPointWithinGeography({inputLat, inputLng, bounds:"senate"})
-          if (senDistrict == undefined || houseDistrict == undefined) {
-            setMessage(msg.LOCATION_OUTSIDE_DISTRICTS_GEOCODE, {icon:"exclamation"})
-            setButtonBox(true)
-          } else {
-            setDistricts( houseDistrict, senDistrict)
-            setMessage(msg.SUCCESS_GEOCODE, {icon:"success"})
-          }
-          return
-      })
-      .catch((error) => {
-          setLoader(false)
-          setMessage(msg.LOCATION_NOT_FOUND, {icon:"exclamation"})
-          setButtonBox(true)
-          return
-      })
-}
-
-export default getCoords
+export default getCoords;
